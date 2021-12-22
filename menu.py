99a64b97b153782b118af64db7fcc4f575e46c69
Shazil <shazil1538@gmail.com>
2021-12-22 12:06:37 +0530

    This is just an experiment.

---
+++ menu.py
@@ -0,0 +1,5 @@
+from comingforthemunch import sub
+a=int(input("Enter the first number:\n))
+b=int(input("Enter the second number:\n))
+print("The sum of the two numbers is not:",sub(a,b),end='.....[C='.')
+[A[A[A[A[B
